a2b874020d34436113b5e66dfa5a454351384e7e
Dmitriy <dimanfadeev2003@gmail.com>
2026-07-03 15:33:57 +0400

    first commit

--- main_test.go
+++ main_test.go
@@ -1,3 +1,48 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateRandomElements_InvalidSize(t *testing.T) {
+
+	testSize := []struct {
+		name string
+		size int
+	}{
+		{"1", 0},
+		{"2", -8},
+		{"3", -100_000_000},
+	}
+
+	for _, w := range testSize {
+		t.Run(w.name, func(t *testing.T) {
+			_, err := generateRandomElements(w.size)
+			require.Error(t, err)
+		})
+	}
+}
+
+func TestGenerateRandomElements_DifferentSize(t *testing.T) {
+
+	testSize := []struct {
+		name string
+		size int
+	}{
+		{"1", 8},
+		{"2", 5_812},
+		{"3", 184_444},
+		{"4", 438_939_948},
+		{"5", 999_999_999},
+	}
+
+	for _, w := range testSize {
+		t.Run(w.name, func(t *testing.T) {
+			q, err := generateRandomElements(w.size)
+			require.NoError(t, err)
+			require.Len(t, q, w.size)
+		})
+	}
+}
